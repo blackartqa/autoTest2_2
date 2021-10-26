3e7ee8f3f17a4ece436ef16d099ff643c36a2428
Artem Chernyshov <black_art@list.ru>
2021-10-26 18:27:12 +0300

    test added

--- src/test/java/ru/netology/TestPostmanEcho.java
+++ src/test/java/ru/netology/TestPostmanEcho.java
@@ -2,7 +2,25 @@ package ru.netology;
 
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class TestPostmanEcho {
     @Test
+    void shouldReturnPostRequest() {
+        //Подход Given-When-Then
+        //Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                //Выполняемые действия
+                .when()
+                .post("/post")
+                //Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
 
 }
